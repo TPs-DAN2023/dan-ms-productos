9f6f10a2d5ccf7f4d6cbcab8d2605ce58abe09f4
ChortJulio <chortjulio@gmail.com>
2023-11-04 17:56:18 -0300

    Add files, folders and migration file
    Agregue un ordenamiento de las carpetas dentro de una src. Tambien anexo el archivo migration_lock el cual crea automaticamente prisma al instanciarlo y la migration.sql que me hizo del schema que viene con el repo. En el index.js hice una prueba para ver si tenia bien instalado NodeJS.

---
+++ index.js
@@ -0,0 +1,19 @@
+// Esta es una prueba que se encuentra en el power NodeJS 
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hola Mundo');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`El servidor se esta ejecutando en http://${hostname}:${port}/`)
+});
+
+
+
+
